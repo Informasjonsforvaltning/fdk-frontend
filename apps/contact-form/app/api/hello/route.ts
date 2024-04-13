6eac2a8aea56694a217c04481270b383bd67b7b0
Pooria Mehregan <pooriamehregan@gmail.com>
2024-04-13 14:52:37 +0200

    fix: eslint config

--- apps/contact-form/app/api/hello/route.ts
+++ apps/contact-form/app/api/hello/route.ts
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-unused-vars
-export async function GET(request: Request) {
+export const GET = async (request: Request) => {
   return new Response('Hello, from API!');
-}
+};
