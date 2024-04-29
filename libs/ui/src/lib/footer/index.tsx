80da80f3c6e31ea21d7c5653e4d8dc6ab9526dc5
Pooria Mehregan <pooriamehregan@gmail.com>
2024-04-29 15:48:26 +0200

    fix: correct svg name imports

--- libs/ui/src/lib/footer/index.tsx
+++ libs/ui/src/lib/footer/index.tsx
@@ -4,8 +4,8 @@ import { getFooterData } from './data';
 import { ColumnData, FooterColumn } from './components/footer-column';
 import styles from './footer.module.css';
 import Image from 'next/image';
-import Ellipse from './images/ellipse.svg';
-import Rectangle from './images/rectangle.svg';
+import Ellipse from './images/Ellipse.svg';
+import Rectangle from './images/Rectangle.svg';
 import { Dictionary } from '@fdk-frontend/dictionaries';
 
 type FooterProps = {
