2560fe8e74bffb7f1d039734fca70037db83fa02
Pooria Mehregan <pooriamehregan@gmail.com>
2024-04-29 17:51:14 +0200

    fix: set correct build output dir

--- apps/forms-e2e/playwright.config.ts
+++ apps/forms-e2e/playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   },
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'yarn nx start forms',
+    command: 'yarn nx dev forms',
     url: 'http://127.0.0.1:3000',
     reuseExistingServer: !process.env.CI,
     cwd: workspaceRoot,
