ef5023231b019aafd6aee45bab16831a7816bbea
Pooria Mehregan <pooriamehregan@gmail.com>
2024-06-04 09:16:29 +0200

    feat(data-hunter): alert on form submit

--- libs/utils/src/index.ts
+++ libs/utils/src/index.ts
@@ -1,4 +1,3 @@
 // Use this file to export React client components (e.g. those with 'use client' directive) or other non-server utilities
 export * from './lib/paths';
-export * from './lib/functions/value-extractors';
 export * from './lib/form-utils';
