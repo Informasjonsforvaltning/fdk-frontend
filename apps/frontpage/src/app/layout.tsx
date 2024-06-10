c07e4846e1e6f8b1428c753d794bd586ddc9c325
Eirik Lillebo <erlillebo@gmail.com>
2024-06-10 10:31:30 +0200

    fix: added postcss config to frontpage

--- apps/frontpage/src/app/layout.tsx
+++ apps/frontpage/src/app/layout.tsx
@@ -1,5 +1,8 @@
 import { RootLayout, RootLayoutProps, generateStaticParams, getMetadata } from './components/layout-root';
 
+import "@digdir/designsystemet-theme";
+import "@digdir/designsystemet-css";
+
 const metadata = getMetadata('Felles datakatalog', 'Felles datakatalog');
 
 export { generateStaticParams, metadata };
