500f76a9964f96b09ce1fd69e69282f2e2370e8c
Eirik Lillebo <erlillebo@gmail.com>
2024-08-15 14:17:53 +0200

    fix: updated meta title/desc values + i18n support

--- apps/forms/app/[lang]/layout.tsx
+++ apps/forms/app/[lang]/layout.tsx
@@ -1,9 +1,4 @@
 import RootLayout, { generateStaticParams } from '@fdk-frontend/ui/layout-root';
 
-const metadata = {
-    title: 'data.norge.no - Datajegeren',
-    description: 'Felles datakatalog',
-};
-
-export { generateStaticParams, metadata };
+export { generateStaticParams };
 export default RootLayout;
--- apps/frontpage/src/app/[lang]/layout.tsx
+++ apps/frontpage/src/app/[lang]/layout.tsx
@@ -1,12 +1,6 @@
 import RootLayout, { RootLayoutProps, generateStaticParams } from '@fdk-frontend/ui/layout-root';
 
-const metadata = {
-    title: 'data.norge.no - Forside',
-    description:
-        ' data.norge.no er det offentlige nettstedet som gir oversikt over beskrivelser av datasett, begrep, api-er og informasjonsmodeller. Innholdet blir levert av ulike virksomheter, offentlige og private. Det er Digitaliseringsdirektoratet som er ansvarlig for drift og utvikling av nettstedet.',
-};
-
 const FrontpageLayout = (props: RootLayoutProps) => <RootLayout {...props} />;
 
 export default FrontpageLayout;
-export { generateStaticParams, metadata };
+export { generateStaticParams };
