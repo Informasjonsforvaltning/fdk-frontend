61967811dca4e5afe44ee778259164c62ad2647f
Eirik Lillebo <erlillebo@gmail.com>
2024-08-22 12:15:49 +0200

    fix: fixed scrollButton indicator to consider hero banner variable height, and to disappear when user scrolls past

--- apps/frontpage/src/app/[lang]/components/frontpage-banner/components/scroll-button/index.tsx
+++ apps/frontpage/src/app/[lang]/components/frontpage-banner/components/scroll-button/index.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import cn from 'classnames';
 
 import { ChevronDownIcon } from '@navikt/aksel-icons';
 
@@ -8,16 +9,46 @@ import styles from './scroll-button.module.scss';
 
 const ScrollButton = () => {
 
+	const [ hidden, setHidden ] = useState(false);
+
 	const scroll = () => {
-		window.scrollTo({
-			top: 200,
-			left: 0,
-			behavior: 'smooth'
-		});
+		const banner = document.getElementById("frontpage-banner");
+		if (!banner) return;
+
+		const scrollTo = banner.offsetHeight - window.innerHeight + 200;
+
+		if (window.scrollY < scrollTo) {
+			window.scrollTo({
+				top: scrollTo,
+				left: 0,
+				behavior: 'smooth'
+			});
+		}
 	}
 
+	const handleScroll = () => {
+		const banner = document.getElementById("frontpage-banner");
+		if (!banner) return;
+
+		const scrollTarget = banner.offsetHeight - window.innerHeight + 200;
+		const hasScrolledPastTarget = window.scrollY > scrollTarget;
+		if (hasScrolledPastTarget) {
+			setHidden(true);
+		}
+	}
+
+	useEffect(() => {
+		window.addEventListener('scroll', handleScroll);
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		}
+	}, []);
+
 	return (
-		<button onClick={scroll} className={styles.scrollButton}>
+		<button
+			onClick={scroll}
+			className={cn(styles.scrollButton, { [styles.hidden]: hidden })}
+		>
 			<ChevronDownIcon className={styles.arrow} />
 			<span>Scroll</span>
 		</button>
