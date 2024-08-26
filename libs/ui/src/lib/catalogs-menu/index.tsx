18513fab0f0990a53ad4dd94ef6b38336a041fc9
Jeff Reiffers <jeffreiffers@gmail.com>
2024-08-26 13:27:53 +0200

    chore: remove nonce for motion

--- libs/ui/src/lib/catalogs-menu/index.tsx
+++ libs/ui/src/lib/catalogs-menu/index.tsx
@@ -1,11 +1,10 @@
 'use client';
 
-import { motion, MotionConfig } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { Link, Heading, Card, Paragraph } from '@digdir/designsystemet-react';
 
 import { Dictionary } from '@fdk-frontend/dictionaries';
 import { CatalogTypes } from '@fdk-frontend/types';
-import { useNonce } from '../csp';
 import CatalogSymbol from '../catalog-symbol/';
 import getMainMenuData from '../main-menu/data';
 
@@ -17,7 +16,6 @@ type CatalogsMenuProps = {
 };
 
 const CatalogsMenu = ({ dictionary, baseUri }: CatalogsMenuProps) => {
-    const nonce = useNonce();
     const data = getMainMenuData(dictionary, baseUri);
 
     const animations = {
@@ -38,46 +36,44 @@ const CatalogsMenu = ({ dictionary, baseUri }: CatalogsMenuProps) => {
 
     return (
         <div className={styles.catalogsMenu}>
-            <MotionConfig nonce={nonce}>
-                <motion.ul
-                    variants={animations.catalogList}
-                    initial='hidden'
-                    animate='show'
-                >
-                    {data.catalogs.map((item: any, i: number) => (
-                        <motion.li
-                            variants={animations.catalogItem}
-                            key={item.key}
+            <motion.ul
+                variants={animations.catalogList}
+                initial='hidden'
+                animate='show'
+            >
+                {data.catalogs.map((item: any, i: number) => (
+                    <motion.li
+                        variants={animations.catalogItem}
+                        key={item.key}
+                    >
+                        <Card
+                            className={styles.card}
+                            asChild
+                            isLink
                         >
-                            <Card
-                                className={styles.card}
-                                asChild
-                                isLink
-                            >
-                                <Link href={item.href}>
-                                    <Card.Header>
-                                        <Heading
-                                            className={styles.catalogTitle}
-                                            size='xxsmall'
-                                            level={3}
-                                        >
-                                            <CatalogSymbol
-                                                className={styles.catalogIcon}
-                                                catalog={item.key as CatalogTypes}
-                                            />
-                                            <span>{item.title}</span>
-                                        </Heading>
-                                    </Card.Header>
-                                    <Card.Content>
-                                        <Paragraph size='small'>{item.description}</Paragraph>
-                                    </Card.Content>
-                                </Link>
-                            </Card>
-                        </motion.li>
-                    ))}
-                </motion.ul>
-            </MotionConfig>
-        </div>        
+                            <Link href={item.href}>
+                                <Card.Header>
+                                    <Heading
+                                        className={styles.catalogTitle}
+                                        size='xxsmall'
+                                        level={3}
+                                    >
+                                        <CatalogSymbol
+                                            className={styles.catalogIcon}
+                                            catalog={item.key as CatalogTypes}
+                                        />
+                                        <span>{item.title}</span>
+                                    </Heading>
+                                </Card.Header>
+                                <Card.Content>
+                                    <Paragraph size='small'>{item.description}</Paragraph>
+                                </Card.Content>
+                            </Link>
+                        </Card>
+                    </motion.li>
+                ))}
+            </motion.ul>
+        </div>
     );
 };
 
