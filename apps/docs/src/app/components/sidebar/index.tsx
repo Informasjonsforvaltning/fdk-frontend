565369099b135df667bd91b2c0604d7958cbd012
Eirik Lillebo <erlillebo@gmail.com>
2024-09-13 13:46:01 +0200

    fix(docs): added remark for table support, sidebar toggling working, styles, pre-responsive

--- apps/docs/src/app/components/sidebar/index.tsx
+++ apps/docs/src/app/components/sidebar/index.tsx
@@ -1,13 +1,116 @@
 'use client';
 
-import { useMemo } from 'react';
+import React, { useState } from 'react';
 import { usePathname } from 'next/navigation';
+import cn from 'classnames';
 
-import { Link, Heading } from '@digdir/designsystemet-react';
+import { Link, Heading, Button } from '@digdir/designsystemet-react';
+import { ChevronDownIcon, ChevronUpIcon } from '@navikt/aksel-icons';
 
 import { type LocaleCodes, type Dictionary } from '@fdk-frontend/dictionaries';
 
 import styles from './sidebar.module.scss';
+
+const buildNestedStructure = (data: Dictionary, basePath: string, locale: LocaleCodes): SidebarItemObject[] => {
+    const items: SidebarItemObject[] = Object.entries(data)
+        .filter(([path]) => path.startsWith(basePath))
+        .map(([path, title]) => ({
+            path,
+            title,
+            level: path.replace(basePath, '').split('/').length - 1,
+        }));
+
+    const nested: SidebarItemObject[] = [];
+    const map: Record<string, SidebarItemObject & { children: SidebarItemObject[] }> = {};
+
+    items.forEach((item) => {
+        map[item.path] = { ...item, children: [] };
+        if (item.level === 0) {
+            nested.push(map[item.path]);
+        } else {
+            const parentPath = item.path.split('/').slice(0, -1).join('/');
+            map[parentPath]?.children.push(map[item.path]);
+        }
+    });
+
+    return nested;
+};
+
+type NestedListProps = {
+    items: SidebarItemObject[];
+    locale: LocaleCodes;
+    pathname: string;
+};
+
+const NestedList = ({ items, locale, pathname }: NestedListProps) => {
+    const [openItems, setOpenItems] = useState<Record<string, boolean>>(() => {
+        const initialOpenState: Record<string, boolean> = {};
+        items.forEach((item) => {
+            if (pathname.includes(item.path)) {
+                initialOpenState[item.path] = true;
+            }
+        });
+        return initialOpenState;
+    });
+
+    const toggleOpen = (itemPath: string) => {
+        setOpenItems((prev) => ({
+            ...prev,
+            [itemPath]: !prev[itemPath],
+        }));
+    };
+
+    const getToggleButton = (isOpen: boolean, itemPath: string) => (
+        <Button
+            variant='tertiary'
+            onClick={() => toggleOpen(itemPath)}
+        >
+            {isOpen ? (
+                <ChevronUpIcon
+                    aria-hidden
+                    fontSize='1.5rem'
+                />
+            ) : (
+                <ChevronDownIcon
+                    aria-hidden
+                    fontSize='1.5rem'
+                />
+            )}
+        </Button>
+    );
+
+    return (
+        <ul className={styles.sidebarList}>
+            {items.map((item) => {
+                const itemPathWithLocale = `/${locale}${item.path}`;
+                const isOpen = openItems[item.path] || false;
+
+                return (
+                    <li
+                        key={item.path}
+                        className={cn({ [styles.itemOpen]: isOpen })}
+                    >
+                        <div>
+                            {itemPathWithLocale === pathname ? (
+                                <strong>{item.title}</strong>
+                            ) : (
+                                <a href={itemPathWithLocale}>{item.title}</a>
+                            )}
+                            {item.children && item.children.length > 0 && getToggleButton(isOpen, item.path)}
+                        </div>
+                        {isOpen && item.children && item.children?.length > 0 && (
+                            <NestedList
+                                items={item.children}
+                                locale={locale}
+                                pathname={pathname}
+                            />
+                        )}
+                    </li>
+                );
+            })}
+        </ul>
+    );
+};
 
 export type SidebarProps = {
     dictionary: Dictionary;
@@ -19,49 +122,13 @@ type SidebarItemObject = {
     path: string;
     title: string;
     level: number;
+    children?: SidebarItemObject[];
 };
 
 const Sidebar = ({ dictionary, slug, locale }: SidebarProps) => {
     const pathname = usePathname();
     const basePath = `/${slug.at(0)}`;
-    const basePathWithLocale = `/${locale}${basePath}`;
-
-    const items: SidebarItemObject[] = Object.entries(dictionary.titles as Record<string, string>)
-        .filter(([path, title]) => path.startsWith(basePath))
-        .map(([path, title]) => ({
-            path,
-            title,
-            level: path.replace(basePath, '').split('/').length - 1,
-        }));
-
-    const renderList = (items: SidebarItemObject[], level = 1) => {
-        return (
-            <ul className={styles.sidebarList}>
-                {items
-                    .filter((item) => item.level === level)
-                    .map((item) => {
-                        const childItems = items.filter(
-                            (child) => child.path.startsWith(item.path) && child.level === level + 1,
-                        );
-
-                        const itemPathWithLocale = `/${locale}${item.path}`;
-
-                        return (
-                            <li key={item.path}>
-                                {itemPathWithLocale === pathname ? (
-                                    <strong>{item.title}</strong>
-                                ) : (
-                                    <a href={itemPathWithLocale}>{item.title}</a>
-                                )}
-                                {childItems.length > 0 && renderList(items, level + 1)}
-                            </li>
-                        );
-                    })}
-            </ul>
-        );
-    };
-
-    const sidebarContent = useMemo(() => renderList(items), [items, locale]);
+    const nestedData = buildNestedStructure(dictionary.titles, basePath, locale);
 
     return (
         <div className={styles.sidebar}>
@@ -69,9 +136,13 @@ const Sidebar = ({ dictionary, slug, locale }: SidebarProps) => {
                 level={2}
                 size='xs'
             >
-                <Link href={`/${locale}${items?.at(0)?.path}`}>{items?.at(0)?.title}</Link>
+                <Link href={`/${locale}${nestedData?.[0]?.path}`}>{nestedData?.[0]?.title}</Link>
             </Heading>
-            {sidebarContent}
+            <NestedList
+                items={nestedData[0]?.children || []}
+                locale={locale}
+                pathname={pathname}
+            />
         </div>
     );
 };
