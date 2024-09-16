11357394339bb45f6815e3b65d69d38c5c7d847b
Jeff Reiffers <jeffreiffers@gmail.com>
2024-09-16 14:38:30 +0200

    fix: added default redirect (prevent 404) and use dev instead of production build

--- apps/docs-e2e/playwright.config.ts
+++ apps/docs-e2e/playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
     },
     /* Run your local dev server before starting the tests */
     webServer: {
-        command: 'yarn nx start docs',
+        command: 'yarn nx dev docs',
         url: 'http://127.0.0.1:3000',
         reuseExistingServer: !process.env.CI,
         cwd: workspaceRoot,
