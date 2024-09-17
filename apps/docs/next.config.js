3af4c24b4cd70ccf9defe259a934b67cd7b3c3be
Jeff Reiffers <jeffreiffers@gmail.com>
2024-09-17 12:23:10 +0200

    chore: use assetprefix instead of basepath

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,7 +1,5 @@
 //@ts-check
 
-const devMode = process.env.NODE_ENV === 'development';
-
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next');
 const createMDX = require('@next/mdx');
@@ -17,17 +15,7 @@ const nextConfig = {
     },
     // Configure `pageExtensions` to include markdown and MDX files
     pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
-    basePath: devMode ? '' : '/docz',
-    async redirects() {
-        return devMode ? [] : [
-            {
-                source: '/',
-                destination: '/docz',
-                basePath: false,
-                permanent: false,
-            },
-        ];
-    }    
+    assetPrefix: '/nb/docs',
 };
 
 // Create the MDX configuration
