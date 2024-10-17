e6dd2e75259235dae5d37d1440964254d0dc91e6
Eirik Lillebo <erlillebo@gmail.com>
2024-10-18 00:05:10 +0200

    fix(details): added details poc pages

---
+++ apps/data-norge/src/app/components/details/badge/index.tsx
@@ -0,0 +1,11 @@
+import { PropsWithChildren } from 'react';
+
+import styles from './badge.module.scss';
+
+const Badge = ({ children, ...props }: PropsWithChildren) => (
+    <div className={styles.badge}>
+        {children}
+    </div>
+);
+
+export default Badge;
