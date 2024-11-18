167b5d6845db73597938f374ecd1b2d26830a4d1
Eirik Lillebo <erlillebo@gmail.com>
2024-11-18 09:20:07 +0100

    fix(details): design community tab

--- libs/ui/src/lib/hstack/index.tsx
+++ libs/ui/src/lib/hstack/index.tsx
@@ -3,7 +3,7 @@ import { PropsWithChildren } from 'react';
 import styles from './hstack.module.scss';
 
 const HStack = ({ children, ...props }: PropsWithChildren) => {
-    return <div className={styles.wrapper}>{children}</div>;
+    return <div className={styles.wrapper} {...props}>{children}</div>;
 };
 
 export default HStack;
---
+++ libs/ui/src/lib/vstack/index.tsx
@@ -0,0 +1,9 @@
+import { PropsWithChildren } from 'react';
+
+import styles from './vstack.module.scss';
+
+const VStack = ({ children, ...props }: PropsWithChildren) => {
+    return <div className={styles.wrapper} {...props}>{children}</div>;
+};
+
+export default VStack;
