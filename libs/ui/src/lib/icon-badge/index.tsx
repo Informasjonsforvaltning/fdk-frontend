77f60d49375b7e22dc902d105aa54b453a4b5980
Eirik Lillebo <erlillebo@gmail.com>
2024-12-09 10:30:32 +0100

    chore(details): types

--- libs/ui/src/lib/icon-badge/index.tsx
+++ libs/ui/src/lib/icon-badge/index.tsx
@@ -3,7 +3,7 @@ import cn from 'classnames';
 
 import styles from './icon-badge.module.scss';
 
-const IconBadge = ({ children, className, fontSize = '1.5rem', ...rest }: React.HTMLAttributes<HTMLDivElement>) => {
+const IconBadge = ({ children, className, fontSize = '1.5rem', ...rest }: React.HTMLAttributes<HTMLDivElement> & { fontSize?: string }) => {
     return (
         <div className={cn(styles.iconBadge, className)} style={{ fontSize }} {...rest}>
             {children}
