4d6ef24e89d3b733ee5eec9696f5f642eb09d81d
Eirik Lillebo <erlillebo@gmail.com>
2025-02-24 13:19:42 +0100

    fix(details): badge stories, tweaks

---
+++ libs/ui/src/lib/star-button/star-button.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { getDictionary } from '@fdk-frontend/dictionaries';
+
+import StarButton from '.';
+import HStack from '../hstack';
+
+const meta: Meta<typeof StarButton> = {
+    component: StarButton,
+    title: 'StarButton',
+};
+
+export default meta;
+type Story = StoryObj<typeof StarButton>;
+
+export const Primary: Story = {
+    parameters: {
+        nextjs: {
+            appDirectory: true,
+        },
+    },
+    render: () => (
+        <>
+            <div style={{ padding: '1rem' }}>
+                <HStack>
+                    <StarButton
+                        labels={['Kopier', 'Kopiert!']}
+                    />
+                </HStack>
+            </div>
+        </>
+    ),
+};
