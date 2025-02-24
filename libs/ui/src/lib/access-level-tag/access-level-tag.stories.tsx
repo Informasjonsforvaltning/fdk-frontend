67a0f9c6fedf5b265c75c9ea4fcdd672bbb8abe3
Eirik Lillebo <erlillebo@gmail.com>
2025-02-24 09:26:15 +0100

    chore(details): Storybook stories

---
+++ libs/ui/src/lib/access-level-tag/access-level-tag.stories.tsx
@@ -0,0 +1,45 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { getDictionary } from '@fdk-frontend/dictionaries';
+
+import AccessLevelTag from '.';
+import VStack from '../vstack';
+
+import dictionary from '../../../../dictionaries/src/lib/dictionaries/nb/details-page.json';
+
+console.log(dictionary);
+
+const meta: Meta<typeof AccessLevelTag> = {
+    component: AccessLevelTag,
+    title: 'AccessLevelTag',
+};
+
+export default meta;
+type Story = StoryObj<typeof AccessLevelTag>;
+
+export const Primary: Story = {
+    parameters: {
+        nextjs: {
+            appDirectory: true,
+        },
+    },
+    render: () => (
+        <>
+            <div style={{ padding: '1rem' }}>
+                <VStack>
+                    <AccessLevelTag
+                        accessCode="PUBLIC"
+                        dictionary={dictionary}
+                    />
+                    <AccessLevelTag
+                        accessCode="RESTRICTED"
+                        dictionary={dictionary}
+                    />
+                    <AccessLevelTag
+                        accessCode="NON_PUBLIC"
+                        dictionary={dictionary}
+                    />
+                </VStack>
+            </div>
+        </>
+    ),
+};
