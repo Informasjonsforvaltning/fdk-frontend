014ce1a12a5726bd6fdb05976df8999d2568a9ac
Eirik Lillebo <erlillebo@gmail.com>
2025-03-03 14:42:49 +0100

    fix(project): not-found page use dictionary

--- apps/data-norge/src/app/[lang]/not-found.tsx
+++ apps/data-norge/src/app/[lang]/not-found.tsx
@@ -1,30 +1,36 @@
 import { type Metadata } from 'next';
+import { headers } from 'next/headers'
+import { i18n, getDictionary, type LocaleCodes } from '@fdk-frontend/dictionaries';
 import { Link, Button, Heading } from '@digdir/designsystemet-react';
 
 import ErrorPage from '../components/error-page';
 
 export default async function NotFound() {
+    const dictionary = await getDictionary('en', 'common');
     return (
         <ErrorPage>
             <Heading
                 level={1}
                 size='medium'
             >
-                404: Siden ble ikke funnet ☹️
+                {dictionary.notFound.title}
             </Heading>
-            <p>Vi klarte dessverre ikke å finne siden du ba om.</p>
+            <p>{dictionary.notFound.message}</p>
             <Button
                 asChild
                 variant='primary'
                 size='small'
             >
-                <Link href='/'>Gå til forsiden</Link>
+                <Link href='/'>{dictionary.notFound.button}</Link>
             </Button>
         </ErrorPage>
     );
 }
 
-export const metadata: Metadata = {
-    title: '404 - data.norge.no',
-    description: 'Siden ble ikke funnet',
+export const generateMetadata = async () => {
+    const dictionary = await getDictionary('en', 'common');
+    return {
+        title: `${dictionary.notFound.title} - data.norge.no`,
+        description: dictionary.notFound.message
+    };
 };
