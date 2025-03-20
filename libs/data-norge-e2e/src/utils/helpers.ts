09ea079991222dc009f29e883ffc3e059ea467ca
Eirik Lillebo <erlillebo@gmail.com>
2025-03-20 22:09:58 +0100

    fix: bump playwright and ignore nextjs elements in a11y tests

--- libs/data-norge-e2e/src/utils/helpers.ts
+++ libs/data-norge-e2e/src/utils/helpers.ts
@@ -2,4 +2,6 @@ import AxeBuilder from '@axe-core/playwright';
 import { Page } from '@playwright/test';
 
 export const generateAccessibilityBuilder = async (page: Page) =>
-    new AxeBuilder({ page }).withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa', 'wcag22aa', 'best-practice']);
+    new AxeBuilder({ page })
+        .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa', 'wcag22aa', 'best-practice'])
+        .exclude([['nextjs-portal', '.nextjs-toast']]);
