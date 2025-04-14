425a324f04928f02e93380718ae4b8085985d78a
Jeff Reiffers <jeffreiffers@gmail.com>
2025-04-14 14:48:17 +0200

    chore: e2e config

--- libs/data-norge-e2e/playwright.config.ts
+++ libs/data-norge-e2e/playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
     },
     /* Run your local dev server before starting the tests */
     webServer: {
-        command: 'yarn nx dev data-norge',
+        command: 'yarn nx dev data-norge --configuration=e2e',
         url: 'http://127.0.0.1:3000',
         reuseExistingServer: !process.env.CI,
         cwd: workspaceRoot,
