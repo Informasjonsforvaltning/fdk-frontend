41007ab0770c26440f8222d640b79df90da42bc2
Eirik Lillebo <erlillebo@gmail.com>
2025-05-05 12:20:32 +0200

    fix(docs): split user-guide and technical into separate sections

---
+++ apps/data-norge/src/app/[lang]/technical/page.tsx
@@ -0,0 +1,14 @@
+import DocsPage, { generateMetadata as docsPageMetadata } from '../../components/docs/docs-page';
+
+export const generateMetadata = async (props: any) => {
+    return await docsPageMetadata({ rootContentDirectory: 'technical', ...props });
+};
+
+export default async function Page(props: any) {
+    return (
+        <DocsPage
+            rootContentDirectory='technical'
+            {...props}
+        />
+    );
+}
