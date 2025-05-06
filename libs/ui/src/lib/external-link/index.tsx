ef28317c39c499f94a81818b0cc047fc8f500c0e
Eirik Lillebo <erlillebo@gmail.com>
2025-05-06 09:19:58 +0200

    fix(project): added optional showIcon prop to ExternalLink

--- libs/ui/src/lib/external-link/index.tsx
+++ libs/ui/src/lib/external-link/index.tsx
@@ -1,14 +1,21 @@
 import { Link, type LinkProps } from '@digdir/designsystemet-react';
-// import { ExternalLinkIcon } from '@navikt/aksel-icons';
+import { ExternalLinkIcon } from '@navikt/aksel-icons';
 
-const ExternalLink = ({ children, ...props }: LinkProps) => (
+type ExternalLinkProps = LinkProps & {
+    showIcon?: boolean;
+}
+
+const ExternalLink = ({ children, showIcon, ...props }: ExternalLinkProps) => (
     <Link {...props}>
         {children}
-        {/*<ExternalLinkIcon
-            aria-hidden
-            fontSize='1em'
-            style={{marginLeft:'0.125em'}}
-        />*/}
+        {
+            showIcon &&
+            <ExternalLinkIcon
+                aria-hidden
+                fontSize='1em'
+                style={{marginLeft:'0.125em'}}
+            />
+        }
     </Link>
 );
 
