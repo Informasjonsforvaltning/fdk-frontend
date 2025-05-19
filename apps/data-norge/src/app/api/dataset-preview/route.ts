41dff395081b4cdb3a19c9ebc592b56ddbf6a8b6
Eirik Lillebo <erlillebo@gmail.com>
2025-05-19 14:49:07 +0200

    fix(details): removed hardcoded base uri

--- apps/data-norge/src/app/api/dataset-preview/route.ts
+++ apps/data-norge/src/app/api/dataset-preview/route.ts
@@ -9,19 +9,18 @@ const {
 } = process.env;
 
 export const POST = async function (request: Request) {
-
-    const TEMP_BASE_URI = 'http://localhost:8080';
-
     try {
-        console.log(FDK_BASE_URI, TEMP_BASE_URI, FDK_DATASET_PREVIEW_API_KEY);
-        
+        const baseUri = process.env.NODE_ENV === 'development'
+            ? 'http://localhost:8080'
+            : FDK_BASE_URI;
+
         const { downloadUrl } = await request.json();
         const referer = request.headers.get('referer') ?? '';
 
         console.log('downloadUrl', downloadUrl);
         
         const csrfResponse = await fetchCsrfToken({
-            baseUri: TEMP_BASE_URI,
+            baseUri: baseUri,
             apiKey: `${FDK_DATASET_PREVIEW_API_KEY}`,
             referer 
         });
@@ -29,7 +28,7 @@ export const POST = async function (request: Request) {
         const { token } = await csrfResponse.json();
         const cookies = csrfResponse.headers.getSetCookie();
         const previewData = await fetchDatasetPreview({
-            baseUri: TEMP_BASE_URI,
+            baseUri: baseUri,
             apiKey: `${FDK_DATASET_PREVIEW_API_KEY}`,
             url: downloadUrl,
             rows: 100,
