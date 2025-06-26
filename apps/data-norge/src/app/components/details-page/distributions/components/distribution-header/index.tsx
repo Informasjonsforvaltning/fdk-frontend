dd0b20feef64fbe154b6b50299d154fa2d197f56
Eirik Lillebo <erlillebo@gmail.com>
2025-06-26 08:59:17 +0200

    feat: added TagList

--- apps/data-norge/src/app/components/details-page/distributions/components/distribution-header/index.tsx
+++ apps/data-norge/src/app/components/details-page/distributions/components/distribution-header/index.tsx
@@ -1,8 +1,7 @@
 import { PropsWithChildren } from 'react';
-import mime from 'mime-types';
-import { Tag } from '@digdir/designsystemet-react';
 import { type Distribution } from '@fdk-frontend/fdk-types';
 import { isOpenLicense, printLocaleValue } from '@fdk-frontend/utils';
+import DistributionTags from '@fdk-frontend/ui/distribution-tags';
 import { type LocaleCodes, type Dictionary } from '@fdk-frontend/dictionaries';
 import styles from './distribution-header.module.scss';
 
@@ -22,7 +21,7 @@ const DistributionHeader = ({
 }: DistributionHeaderProps & PropsWithChildren) => {
     const hasOpenLicense = distribution.license && distribution.license.some((l: any) => isOpenLicense(l.uri));
     const hasTags = hasOpenLicense || exampleData || distribution.fdkFormat?.length || hasOpenLicense;
-    const hasApi = distribution.accessService?.length;
+    const hasApi = Boolean(distribution.accessService?.length);
 
     return (
         <div className={styles.headerContent}>
@@ -33,47 +32,12 @@ const DistributionHeader = ({
                       ? distribution.accessURL
                       : dictionary.distributions.header.nameless}
                 {hasTags && (
-                    <div className={styles.tags}>
-                        {hasOpenLicense && (
-                            <Tag
-                                className={styles.tag}
-                                color='success'
-                                size='sm'
-                            >
-                                {dictionary.distributions.header.openLicense}
-                            </Tag>
-                        )}
-                        {hasApi && (
-                            <Tag
-                                className={styles.tag}
-                                color='success'
-                                size='sm'
-                            >
-                                {dictionary.distributions.header.hasApi}
-                            </Tag>
-                        )}
-                        {exampleData && (
-                            <Tag
-                                className={styles.tag}
-                                color='neutral'
-                                size='sm'
-                            >
-                                {dictionary.distributions.header.exampleData}
-                            </Tag>
-                        )}
-                        {distribution.fdkFormat
-                            ?.filter((format: any) => format?.code)
-                            .map((format: any) => (
-                                <Tag
-                                    className={styles.tag}
-                                    color='info'
-                                    size='sm'
-                                    key={format.code}
-                                >
-                                    {(mime.extension(format.code) || format.name || format.code)?.toLowerCase()}
-                                </Tag>
-                            ))}
-                    </div>
+                    <DistributionTags
+                        distribution={distribution}
+                        exampleData={exampleData}
+                        hasApi={hasApi}
+                        dictionary={dictionary}
+                    />
                 )}
             </span>
         </div>
