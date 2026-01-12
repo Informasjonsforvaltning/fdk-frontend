f027030497c6630aa0bf445a4132b089be217e7d
Jeff Reiffers <jeffreiffers@gmail.com>
2026-01-12 08:52:49 +0100

    fix: svgr not supported

--- apps/data-norge/next.config.js
+++ apps/data-norge/next.config.js
@@ -9,11 +9,6 @@ const createMDX = require('@next/mdx');
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
-    nx: {
-        // Set this to true if you would like to use SVGR
-        // See: https://github.com/gregberge/svgr
-        svgr: false,
-    },
     // Configure `pageExtensions` to include markdown and MDX files
     pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
     assetPrefix: '/nb',
