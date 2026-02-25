ba9b6d51809fd18f186493ce674f73dc5ef1d64f
Eirik Lillebo <erlillebo@gmail.com>
2026-02-25 11:06:52 +0100

    fix: entity-card fidling

--- libs/ui/src/lib/entity-teaser/index.tsx
+++ libs/ui/src/lib/entity-teaser/index.tsx
@@ -1,13 +1,37 @@
 import React from 'react';
-import { Card } from '@digdir/designsystemet-react';
+import { Card, Heading, Link, Paragraph } from '@digdir/designsystemet-react';
 import { OrgLogo } from '@fellesdatakatalog/ui';
+import styles from './styles.module.scss';
 
 const EntityTeaser = () => {
     return (
+        // <Card>
+            
+        //     <OrgLogo orgLogoSrc='https://orglogo.digdir.no/api/emblem/svg/991825827' />
+        //     Hello im an entity teaser
+        // </Card>
         <Card>
-            <OrgLogo orgNr="991825827" />
-            <OrgLogo orgLogoSrc='https://orglogo.digdir.no/api/emblem/svg/991825827' />
-            Hello im an entity teaser
+            <Card.Block>
+                <div>
+                    <OrgLogo className={styles.orgLogo} orgNr="991825827" />
+                    <Heading>
+                        <Link
+                            href='https://designsystemet.no'
+                            target='_blank'
+                            rel='noopener noreferrer'
+                        >
+                            Myrkheim Museum
+                        </Link>
+                    </Heading>
+                </div>
+            <Paragraph>
+                Myrkheim Museum ligg i dalen mellom dei gamle fjelltoppane og viser
+                utstillingar frå tida då dei fyrste reisefølgja kryssa landet. Her kan
+                du utforske eldgamle kart, reiskapar frå dei store vandringane og
+                forteljingar bevart av skogvaktarane.
+            </Paragraph>
+            <Paragraph data-size='sm'>Myrkheim Kulturvernråd</Paragraph>
+            </Card.Block>
         </Card>
     );
 }
