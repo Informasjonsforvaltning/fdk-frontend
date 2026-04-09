2e46174d0c43c87d95bb695689c0d4951e7b5e53
Øyvind Julsrud <oyvjul@gmail.com>
2026-04-09 09:58:11 +0200

    fix: restore Label in LabelWithTag for a11y

--- libs/ui/src/lib/label-with-tag/index.tsx
+++ libs/ui/src/lib/label-with-tag/index.tsx
@@ -1,4 +1,4 @@
-import { Tag } from '@digdir/designsystemet-react';
+import { Label, Tag } from '@digdir/designsystemet-react';
 import { HTMLAttributes } from 'react';
 import styles from './label-with-tag.module.css';
 
@@ -12,7 +12,7 @@ const LabelWithTag = ({ labelText, tagText, ...rest }: LabelWithTagProps) => (
         {...rest}
         className={styles.contentContainer}
     >
-        <span>{labelText}</span>
+        <Label>{labelText}</Label>
         {tagText && (
             <Tag
                 data-size='sm'
