402e04f22406d2226b20c4aedd992837ef284a57
Øyvind Julsrud <oyvjul@gmail.com>
2026-04-21 14:23:09 +0200

    style: apply linter formatting to temporalDate util

--- libs/utils/src/lib/temporalDate/index.ts
+++ libs/utils/src/lib/temporalDate/index.ts
@@ -4,13 +4,14 @@ const yearPattern = /^\d{4}$/;
 const yearMonthPattern = /^\d{4}-\d{2}$/;
 const fullDatePattern = /^\d{4}-\d{2}-\d{2}$/;
 
-export const formatTemporalDate = (
-    value: string | undefined | null,
-    locale: LocaleCodes,
-): string => {
-    if (!value) return '';
+export const formatTemporalDate = (value: string | undefined | null, locale: LocaleCodes): string => {
+    if (!value) {
+        return '';
+    }
 
-    if (yearPattern.test(value)) return value;
+    if (yearPattern.test(value)) {
+        return value;
+    }
 
     if (yearMonthPattern.test(value)) {
         const [year, month] = value.split('-').map(Number);
