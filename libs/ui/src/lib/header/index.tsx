97ef3c0c6472cfda18c5ebf1542956ca71332767
kristianeskogvang <kristiane.kolshus@gmail.com>
2026-08-20 11:28:35 +0200

    feat: don't show or render search input tray for transportportal

--- libs/ui/src/lib/header/index.tsx
+++ libs/ui/src/lib/header/index.tsx
@@ -134,6 +134,7 @@ const Header = ({ locale, frontpage, showSearchInput, profile = "data.norge" }: 
             <SearchInput
               locale={locale}
               className={styles.headerSearchInput}
+              showTray={profile !== "transportportal"}
             />
           ) : (
             <div style={{ flexGrow: 1 }} />
